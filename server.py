b95a62e489dd8a8a065b59ac7098f65ad3215d1d
Darren52BT <darrenbt52@gmail.com>
2024-04-03 14:33:22 -0700

    server starter code

---
+++ server.py
@@ -0,0 +1,89 @@
+import socket
+import select
+
+
+#possibly refactor to include message headers like with message lengths
+
+SERVER_IP, SERVER_PORT = "localhost", 12345
+#create server socket, allow for reusability, bind and start listening
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket.bind((SERVER_IP, SERVER_PORT))
+server_socket.listen()
+
+
+#for select
+sockets_list = [server_socket]
+#contains clients
+clients = set()
+
+
+
+def receive_message(client_socket):      
+    #try getting message, if doesn't exist return None, otherwise return data
+    try: 
+        message = client_socket.recv(1024)
+        if not len(message) or len(message) == 0:
+            return None
+        return {'data': message}
+    #otherwise connection is over
+    except: 
+        return None
+    
+
+#loop to infinitely accept client requests
+while True: 
+    read_sockets, write_sockets, exception_sockets = select.select(sockets_list, [], sockets_list)
+
+    #iterate over notified sockets 
+    for notified_socket in read_sockets:
+
+        #if socket is server socket then we have a new connection, only accept if length of clients is 10 or less
+        if notified_socket == server_socket and len(clients) <=10:
+            client_socket, client_address = server_socket.accept()
+
+
+            # #receive message
+            # message = receive_message(client_socket)
+
+            # #if there's no message or connection ended
+            # if message is None:
+            #     continue 
+
+            #otherwise, append client socket to list of sockets to be monitored and list of client sockets
+            sockets_list.append(client_socket) 
+            clients.add(client_socket)
+
+        #otherwise current socket is sending message
+        else:
+            message = receive_message(notified_socket)
+
+            #handle disconnection
+            if message is None:
+                print("Closed connection from ", notified_socket)
+                sockets_list.remove(notified_socket)
+                clients.remove(notified_socket)
+                continue 
+            
+            print("Message from ", notified_socket)
+
+            #send message to every client 
+            for client_socket in clients:
+                client_socket.send(str(notified_socket) + ": " + message['data'])
+
+        #handle any sockets that have any errors
+        for notified_socket in exception_sockets:
+            # Remove from list for socket.socket()
+            sockets_list.remove(notified_socket)
+            # Remove from our list of users
+            clients.remove(notified_socket)
+
+
+
+
+
+
+
+        
+
+    
